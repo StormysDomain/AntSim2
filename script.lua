451c89e803807acda02fd8ef3a3b28773fbb37e2
StormysDomain <81189735+StormysDomain@users.noreply.github.com>
2025-07-11 23:17:00 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -56,9 +56,9 @@ KeyTab:CreateButton({
             local hivePosition = Vector3.new(-42.8689, 5.1038, -324.8359)
             local convertArgs = {"ActionCall", "Anthill", workspace:WaitForChild("Anthills"):WaitForChild("1"):WaitForChild("Platform")}
             local toolArgs = {"UseTool", 1}
-            local tweenSpeed = 0.035
+            local tweenSpeed = 0.020
             local toolInterval = 0.05
-            local hiveWaitTime = 7
+            local hiveWaitTime = 10
 
             local function moveTo(pos)
                 local info = TweenInfo.new(tweenSpeed, Enum.EasingStyle.Linear)
